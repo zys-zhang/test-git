08827903ab0f96392dbbd207052a30bb555973f9
zys-zhang <2607575675@qq.com>
2022-05-08 12:38:42 +0800

    first commit

---
+++ src/com/zys.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author zys
+ * @date 2022/5/8- 12:05
+ */
+public class zys {
+    public static void main(String[] args){
+        System.out.println("hello test");
+    }
+}
